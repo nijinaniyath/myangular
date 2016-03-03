1a7a43e9588295b149e23dc5c0550cf17e5edb89
nijin <nijinaniyath@gmail.com>
2016-03-03 16:25:31 +0530

    commit

--- client/app/global/configuration.constant.js
+++ client/app/global/configuration.constant.js
@@ -4,15 +4,15 @@
 
     angular.module('app.global').
         constant('configuration', {
-            idsrvurl: 'https://www.attinad.com:444/idsrv/issue/',
-            clientid: 'mmclientimplicit',
-            profileuri: 'https://www.attinad.com:444/meetingmanager/api/user',
-            returnuri: 'https://localhost:9000/',
-            scope: 'urn:meetingmanagerservice',
-            authorizepath: 'oauth2/authorize',
-            servicebase: 'https://www.attinad.com:444/meetingmanager/api/',
+            idsrvurl: '',
+            clientid: '',
+            profileuri: '',
+            returnuri: '',
+            scope: '',
+            authorizepath: '',
+            servicebase: '',
             sitebase:'/',
-            signalrbase:'https://www.attinad.com:444/meetingmanager/'
+            signalrbase:''
         });
 
 })();
